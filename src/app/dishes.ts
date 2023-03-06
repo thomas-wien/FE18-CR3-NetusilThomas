63a7034c11b321e26f786305460d94702b6209fb
Thomas Netusil <thomas.netusil@gmail.com>
2023-03-06 15:35:45 +0100

    changed links

--- src/app/dishes.ts
+++ src/app/dishes.ts
@@ -10,7 +10,7 @@ export class dish {
   constructor(name: string, description: string, image: string, price: number, available: boolean, count: number) { // 
     this.name = name;
     this.description = description;
-    this.image = "../../assets/images/dishes/" + image; // 
+    this.image = "./assets/images/dishes/" + image; // 
     this.price = price;
     this.available = available;
     this.count = count;
